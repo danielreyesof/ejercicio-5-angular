f9e71b9140dbe2eed3e7719cd5b7c8223e1b2183
Daniel Reyes <kevinhurtado002@gmail.com>
2022-11-14 23:36:39 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+
+interface Units {
+  unit?: string;
+  value?: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService],
 })
 export class AppComponent {
-  title = 'ejercicio-5-angular';
+  constructor(private messageService: MessageService) {}
+
+  value!: number;
+  cols: any[] = [];
+  values: Units[] = [];
+  showTable: boolean = false;
+
+  verify(): void {
+    this.showTable = false;
+
+    switch (true) {
+      case this.value === 0:
+        this.addSingle('warn', '', 'El numero es 0');
+        break;
+      case this.value >= 1 || this.value <= -1:
+        this.values = [
+          {
+            unit: 'kilometros (km)',
+            value: this.value / 1000,
+          },
+          {
+            unit: 'hectometros (hm)',
+            value: this.value / 100,
+          },
+          {
+            unit: 'decametros (dm)',
+            value: this.value / 10,
+          },
+          {
+            unit: 'centimetros (cm)',
+            value: this.value * 100,
+          },
+          {
+            unit: 'milimetros (mm)',
+            value: this.value * 1000,
+          },
+        ];
+        this.showTable = true;
+        break;
+      default:
+        this.addSingle('error', '', 'Por favor ingrese un numero');
+        break;
+    }
+  }
+
+  addSingle(severity: string, summary: string, detail: string) {
+    this.messageService.add({ severity, summary, detail });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'primeng/accordion';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    ToastModule,
+    TableModule,
+    FormsModule,
+    MessagesModule,
+    MessageModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
